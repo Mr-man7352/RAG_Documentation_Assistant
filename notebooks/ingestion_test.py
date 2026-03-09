932d3f708c82c294142cf5628e0660d646c98732
Manish Sahu <manissh7352@gmail.com>
2026-03-09 03:50:18 +0000

    Fix URL in ingestion_test.py to load specific issues documentation

--- notebooks/ingestion_test.py
+++ notebooks/ingestion_test.py
@@ -2,7 +2,7 @@ from RAG.ingestion.web_loader import GithubDocLoader
 
 loader = GithubDocLoader()
 
-doc= loader.load("https://docs.github.com/en/rest/issues")
+doc= loader.load("https://docs.github.com/en/rest/issues/issues")
 
 print(doc["title"])
 print(doc["content"][:1000])
